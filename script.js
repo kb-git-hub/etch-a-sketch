6b90c594bf9bbbb12e0dd361e1963406022539a9
kj.br <kj.br@proton.me>
2022-08-02 22:59:42 -0500

    update css, js, html

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 const sketchContainer = document.querySelector('.sketchContainer')
+const sketchContainerDims = 500
 
 
-function createGrid(input){
-    for (let row = 0; row < input; row++){
-        for(let col = 0; col < input; col++){
+function createGrid(input) {
+    for (let row = 0; row < input; row++) {
+        for (let col = 0; col < input; col++) {
             let grid = `<div class='gridItem'></div>`
             sketchContainer.innerHTML += grid
-
-            console.log(grid);
-            document.querySelectorAll('.gridItem').forEach(function(element){
-                element.style.width = 500/input + 'px'
-                element.style.height = 500/input + 'px'
-            })
         }
     }
-
-
-    
-    // grid.style.height = `${500/input} + px`
-
+    document.querySelectorAll('.gridItem').forEach(function (element) {
+        element.style.width = sketchContainerDims / input + 'px'
+        element.style.height = sketchContainerDims / input + 'px'
+    })
 }
 
 createGrid(16)
